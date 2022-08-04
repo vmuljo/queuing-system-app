62b1b8939284c4648ae35a862dd0bbfa23ea59e7
Victor Muljo <muljo@usc.edu>
2022-08-03 21:48:13 -0700

    Added clear queue button and individual removing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const modal = document.querySelector(".modal");
+const modal = document.querySelector("#modalForm");
 const modalBlock = document.querySelector("#modal-content");
 const btn = document.querySelector("#btn");
-const close = document.querySelector(".close");
+const close = document.querySelector("#queueclose");
 const list = document.querySelector(".namesList");
 const admin = document.querySelector(".admin-enable");
 var contents = document.querySelector(".contents");
@@ -42,15 +42,18 @@ function adminToggle(){
     contents = document.querySelector('.contents')
     contents.classList.toggle("admin");
     if(contents.classList.contains("admin")){
-        // var button = document.createElement('button');
-        // button.type = "button";
-        // button.id = "btn-remove";
-        // button.innerHTML = "Remove First";
-        // document.querySelector(".buttons").appendChild(button); 
-        document.querySelector('#btn-remove').style.display = "block";
+        const guestoptions = document.querySelectorAll('.guest-option');
+        guestoptions.forEach(guestoption => {
+            guestoption.classList.add('admin-option');
+            guestoption.classList.remove('guest-option');
+        });
     }
     else{
-        document.querySelector("#btn-remove").style.display = 'none';
+        const adminoptions = document.querySelectorAll('.admin-option');
+        adminoptions.forEach(adminoption => {
+            adminoption.classList.add('guest-option');
+            adminoption.classList.remove('admin-option');
+        });
     }
 }
 
@@ -70,6 +73,44 @@ function modalToggle(){
         
     }
 }
+
+function viewGuest(){
+    const guestList = document.querySelectorAll('.queue-entry');
+
+    guestList.forEach(guest => {
+        guest[i].onclick = () => {
+
+        }
+    })
+    for(var i = 0; i<guest.length; i++){
+        guest[i].onclick = () => {
+            console.log(queue)
+        }
+    }
+}
+
+function guestModal(){
+
+}
+
+function removeQueueEntry(){
+    var guestList = document.getElementsByClassName('remove');
+    for(var i = 0; i<guestList.length; i++){
+        guestList[i].onclick = function(){
+            console.log(guestList.length);
+            this.parentNode.remove();
+            if(guestList.length == 0) makeEmpty();
+        }
+    }
+}
+
+function makeEmpty(){
+    list.classList.add("empty");
+    list.innerHTML = "No one is in the Queue!";
+    console.log("No one is in the Queue!");
+}
+
+removeQueueEntry();
 
 document.querySelector("#submitbtn").onclick = function(){
     var name = document.querySelector("#name").value.trim();
@@ -99,19 +140,41 @@ document.querySelector("#submitbtn").onclick = function(){
         var latestPushedName = queue.at(-1).name;
 
         const li = document.createElement('li');
+        const deleteSpan =  document.createElement('span');
+        const readySpan = document.createElement('span');
+        const div_li = document.createElement('div');
+
+        div_li.classList.add('queue-entry');
+        deleteSpan.classList.add('guest-option', 'remove');
+        readySpan.classList.add('guest-option', 'ready');
+        if(document.querySelector('.contents').classList.contains('admin')){
+            deleteSpan.classList.add('admin-option');
+            readySpan.classList.add('admin-option');
+            deleteSpan.classList.remove('guest-option');
+            readySpan.classList.remove('guest-option');
+        }
+
+        deleteSpan.innerHTML = "&#10006;";
+        readySpan.innerHTML = "&#10004;";
         li.innerHTML = latestPushedName;
-        list.appendChild(li);
+
+        div_li.appendChild(readySpan);
+        div_li.appendChild(li);
+        div_li.appendChild(deleteSpan);
+        list.appendChild(div_li);
+
         queueList = document.querySelectorAll(".namesList li");
-        queueArray = Array.from(queueList); 
-        console.log(queueArray);
+        // queueArray = Array.from(queueList); 
+        // console.log(queueArray);
         document.querySelector("#name").value = "";
         document.querySelector("#pNumber").value = "";
         modalToggle();
+        // removeQueueEntry();
     }
 
 }
 
-document.querySelector('#btn-remove').onclick = function(){
+document.querySelector('#btn-remove').onclick = () => {
     var cleared = false;
     // console.log(list);
     console.log(queueList);
@@ -123,19 +186,28 @@ document.querySelector('#btn-remove').onclick = function(){
         console.log(queueArray);
         queueList = document.querySelectorAll(".namesList li");
         if(queueList.length == 0){
-            list.classList.add("empty");
-            list.innerHTML = "No one is in the Queue!";
+            makeEmpty();
         }
     }
     else{
-        list.classList.add("empty");
-        list.innerHTML = "No one is in the Queue!";
-        console.log("No one is in the Queue!");
+        makeEmpty();
         alert("No one is in the queue!");
         
         var cleared = true;
     }
     console.log(queueList.length);
+}
+
+document.querySelector('#btn-clear').onclick = () => {
+    if(queueList.length > 0){
+        list.innerHTML = '';
+        queueList = document.querySelectorAll(".namesList li");
+        queue = [];
+        makeEmpty();
+        return;
+    }
+    console.log("No one is in the Queue!");
+    alert("No one is in the queue!");
 }
 
 btn.onclick = () => {modalToggle();}
